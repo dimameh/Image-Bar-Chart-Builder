d63547e551fbf14558bee21b2310c6df7e559e12
dimameh <codemekh@gmail.com>
2019-10-19 11:50:55 +0700

    Add files via upload

---
+++ GistogrammApp/BarChartBuilderForm.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace BarChartBuilder
+{
+    partial class BarChartBuilderForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BarChartPictureBox = new System.Windows.Forms.PictureBox();
+            this.BrowseImageButton = new System.Windows.Forms.Button();
+            this.ImageNameLabel = new System.Windows.Forms.Label();
+            this.OpenFileDialog = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.BarChartPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BarChartPictureBox
+            // 
+            this.BarChartPictureBox.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.BarChartPictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.BarChartPictureBox.Location = new System.Drawing.Point(12, 12);
+            this.BarChartPictureBox.Name = "BarChartPictureBox";
+            this.BarChartPictureBox.Size = new System.Drawing.Size(346, 317);
+            this.BarChartPictureBox.TabIndex = 0;
+            this.BarChartPictureBox.TabStop = false;
+            // 
+            // BrowseImageButton
+            // 
+            this.BrowseImageButton.Location = new System.Drawing.Point(12, 335);
+            this.BrowseImageButton.Name = "BrowseImageButton";
+            this.BrowseImageButton.Size = new System.Drawing.Size(75, 30);
+            this.BrowseImageButton.TabIndex = 1;
+            this.BrowseImageButton.Text = "Обзор...";
+            this.BrowseImageButton.UseVisualStyleBackColor = true;
+            this.BrowseImageButton.Click += new System.EventHandler(this.BrowseImageButton_Click);
+            // 
+            // ImageNameLabel
+            // 
+            this.ImageNameLabel.AutoSize = true;
+            this.ImageNameLabel.Location = new System.Drawing.Point(9, 368);
+            this.ImageNameLabel.Name = "ImageNameLabel";
+            this.ImageNameLabel.Size = new System.Drawing.Size(321, 17);
+            this.ImageNameLabel.TabIndex = 2;
+            this.ImageNameLabel.Text = "Выберите или перетащите .png или .jpeg файл";
+            // 
+            // OpenFileDialog
+            // 
+            this.OpenFileDialog.FileName = "Выберите файл";
+            this.OpenFileDialog.Filter = "Image Files (JPEG,JPG,PNG)|*.JPG;*.JPEG;*.PNG";
+            // 
+            // BarChartBuilderForm
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(368, 393);
+            this.Controls.Add(this.ImageNameLabel);
+            this.Controls.Add(this.BrowseImageButton);
+            this.Controls.Add(this.BarChartPictureBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "BarChartBuilderForm";
+            this.Text = "BarChartBuilder";
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.BarChartBuilderForm_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.BarChartBuilderForm_DragEnter);
+            ((System.ComponentModel.ISupportInitialize)(this.BarChartPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox BarChartPictureBox;
+        private System.Windows.Forms.Button BrowseImageButton;
+        private System.Windows.Forms.Label ImageNameLabel;
+        private System.Windows.Forms.OpenFileDialog OpenFileDialog;
+    }
+}
+
---
+++ GistogrammApp/BarChartBuilderForm.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace BarChartBuilder
+{
+    public partial class BarChartBuilderForm : Form
+    {
+        #region Constants
+
+        /// <summary>
+        ///     Количество уровней яркости
+        /// </summary>
+        private const int BrightnessLevelsCount = 256;
+
+        #endregion
+
+        #region Private fields
+
+        /// <summary>
+        ///     Холст
+        /// </summary>
+        private Bitmap _bitmap;
+
+        /// <summary>
+        ///     Массив расчитанных значений яркости
+        /// </summary>
+        private int[] _brightnessArray = new int[BrightnessLevelsCount];
+
+        #endregion
+
+        #region Constructor
+
+        public BarChartBuilderForm()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Private methods
+
+        /// <summary>
+        ///     Нарисовать гистограмму
+        /// </summary>
+        private void DrawBarChart()
+        {
+            _bitmap = new Bitmap(BrightnessLevelsCount, BrightnessLevelsCount);
+
+            //Отрисовщик графики
+            var graphics = Graphics.FromImage(_bitmap);
+
+            //Цвет отрисовки
+            var pen = new Pen(Color.Black);
+
+            //Отрисовка гистограммы на холсте
+            for (var i = 0; i < BrightnessLevelsCount; i++)
+            {
+                graphics.DrawLine(pen, i, BrightnessLevelsCount, i,
+                    BrightnessLevelsCount - _brightnessArray[i]);
+            }
+
+            BarChartPictureBox.Image = _bitmap;
+        }
+
+        /// <summary>
+        ///     Расчет и подгонка значений яркости
+        /// </summary>
+        /// <param name="filePath">Расположение файла</param>
+        private void BuildImageBarChart(string filePath)
+        {
+            var image = new Bitmap(filePath);
+
+            CalculateBarChart(image);
+
+            image.Dispose();
+
+            FitBrightnessArrayValues();
+        }
+
+        /// <summary>
+        ///     Расчет и отрисовка гистограммы
+        /// </summary>
+        /// <param name="filePath">Расположение файла</param>
+        private void BuildAndDrawBarChart(string filePath)
+        {
+            try
+            {
+                ImageNameLabel.Text = Path.GetFileName(filePath);
+
+                BuildImageBarChart(filePath);
+
+                DrawBarChart();
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message, "Ошибка",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                ImageNameLabel.Text = "Выберите или перетащите .png или .jpeg файл";
+            }
+        }
+
+        /// <summary>
+        ///     Расчитать гистограмму картинки
+        /// </summary>
+        /// <param name="image">Изображение для которого нужно расчитать гистограмму</param>
+        private void CalculateBarChart(Bitmap image)
+        {
+            _brightnessArray = new int[BrightnessLevelsCount];
+
+            for (var i = 0; i < image.Width; i++)
+            {
+                for (var j = 0; j < image.Height; j++)
+                {
+                    var pixel = image.GetPixel(i, j);
+                    pixel.GetBrightness();
+
+                    //Получаем значение уровня яркости пикселя по формуле
+                    var brightnessLevel = 0.3 * pixel.R + 0.59 * pixel.G + 0.11 * pixel.B;
+
+                    //Округляем до целых чисел и конвертируем в int значение
+                    var arrayLevel = Convert.ToInt32(Math.Round(brightnessLevel,
+                        MidpointRounding.AwayFromZero));
+
+                    //Добавляем одно значение
+                    _brightnessArray[arrayLevel]++;
+                }
+            }
+        }
+
+        /// <summary>
+        ///     Подогнать значения массива под размеры холста
+        /// </summary>
+        private void FitBrightnessArrayValues()
+        {
+            //Уменьшаем все значения в два раза пока максимальное не станет меньше высоты холста
+            while (_brightnessArray.Max() > BarChartPictureBox.Height)
+            {
+                for (var i = 0; i < BrightnessLevelsCount; i++)
+                {
+                    _brightnessArray[i] /= 2;
+                }
+            }
+        }
+
+        /// <summary>
+        ///     Обработка нажатия на BrowseImageButton
+        /// </summary>
+        private void BrowseImageButton_Click(object sender, EventArgs e)
+        {
+            if (OpenFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                BuildAndDrawBarChart(OpenFileDialog.FileName);
+            }
+        }
+
+        #endregion
+
+        #region DragNDrop
+
+        private void BarChartBuilderForm_DragEnter(object sender, DragEventArgs e)
+        {
+            //Изменение вида курсора при перетаскивании файла
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+        }
+
+        private void BarChartBuilderForm_DragDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                //Всегда оставляем только первый файл если их перетащили несколько
+                var filePath = ((string[]) e.Data.GetData(DataFormats.FileDrop))[0];
+
+                var fileExtension = Path.GetExtension(filePath)?.ToLower();
+                if (fileExtension != ".jpeg" && fileExtension != ".jpg" &&
+                    fileExtension != ".png")
+                {
+                    MessageBox.Show("Неверный формат файла.", "Ошибка",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    BuildAndDrawBarChart(filePath);
+                }
+            }
+        }
+
+        #endregion
+    }
+}
